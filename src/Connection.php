f16dd72541d25070baff883dd3fbf883edea3f64
Alexander <kichatovay@gmail.com>
2017-10-17 16:46:57 +0400

    cancel change

--- src/Connection.php
+++ src/Connection.php
@@ -201,7 +201,7 @@ class Connection
         $dsn = "mysql:host={$this->host};dbname={$this->database};charset={$this->charset}";
 
         $options = [
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES   => true,
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
